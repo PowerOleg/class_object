9495278a60e60819ee2cf0385bdce2be93900447
PowerOleg <geraskin.oleg@mail.ru>
2024-02-25 11:01:06 +0300

    task2;loop

--- task2/Project2/Project2/main.cpp
+++ task2/Project2/Project2/main.cpp
@@ -15,14 +15,53 @@ public:
 		this->count = initial_count;
 	}
 
+	bool increment()
+	{
+		this->count++;
+	}
 
+	bool decrement()
+	{
+		this->count--;
+	}
 
+	bool print_count()
+	{
+		std::cout << this->count << std::endl;
+	}
 };
+
+void loop(Counter counter)
+{
+	char input3 = '0';
+	do
+	{
+		std::cout << "Введите команду('+', '-', '=' или 'x'): " << std::endl;
+		std::cin >> input3;
+		std::cout << counter.print_count();
+	} while (input3 != 'x');
+}
 
 int main(int argc, char** argv)
 {
 	setlocale(LC_ALL, "ru");
-	Counter counter(12);
+	
+	std::cout << "Вы хотите указать начальное значение счётчика? Введите да или нет: " << std::endl;
+	char input1[7]{"нет"};
+	std::cin >> /*std::setw(4) >>*/ input1;
+	if (strcmp(input1, "да"))
+	{
+		std::cout << "Введите начальное значение счётчика: " << std::endl;
+		int input2 = 0;
+		std::cin >> input2;
+		Counter counter1(input2);
+		loop(counter1);
+	}
+	else
+	{
+		Counter counter2;
+		loop(counter2);
+	}
 
 	return 0;
 }
